a349e329c8b0c95a73367b4c904971d44a856693
Jeff Swensen <jeffswensen@gmail.com>
2013-08-12 23:36:02 -0400

    still fixing url

--- taps.rb
+++ taps.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 
-put 'taps' do
+put '/taps' do
   data = JSON.parse(request.body.string)
   if data.nil? or !data.has_key?('nid') or !data.has_key?('on') or !data.has_key?('off') then
     status 400
