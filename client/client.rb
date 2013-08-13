78c360f5916424eb37e9da5ddc6f61a7c5562f60
Jeff Swensen <jeffswensen@gmail.com>
2013-08-12 23:34:19 -0400

    fixing URL

--- client/client.rb
+++ client/client.rb
@@ -1,5 +1,6 @@
-require 'rest-client'
+require 'rest_client'
+require 'json'
 
-RestClient.put 'http://alytics.herokuapp.com/', 
+RestClient.put 'http://alytics.herokuapp.com/taps', 
   { 'nid' => 'fakenid', 'on' => 'fakeon', 'off' => 'fakeoff' }.to_json, 
   :content_type => :json
